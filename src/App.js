fe05b8f3869805b9ce54a9299eaab9523d495639
Alexey <elexex97@gmail.com>
2019-07-07 23:09:33 +0300

    FEATURE: Home & NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Havbar from './components/layout/Havbar';
-import Users from './components/users/Users';
+import Home from './components/pages/Home';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+
 import './App.css';
 
 function App() {
@@ -20,18 +22,10 @@ function App() {
             <div className='container'>
               <Alert alert={alert} />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={() => (
-                    <>
-                      <Search />
-                      <Users />
-                    </>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/user/:login' component={User} />
                 <Route exact path='/about' component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Users from '../users/Users';
+import Search from '../users/Search';
+
+function Home() {
+  return (
+    <>
+      <Search />
+      <Users />
+    </>
+  );
+}
+
+export default Home;
