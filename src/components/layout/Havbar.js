8e73b0975507cb79d39a43f81aad18f4f7c8f837
Alexey <elexex97@gmail.com>
2019-07-07 16:52:03 +0300

    CLEANING: Stateless Functional Components

--- src/components/layout/Havbar.js
+++ src/components/layout/Havbar.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Havbar extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    icon: PropTypes.string
-  };
-
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github'
-  };
-
-  render() {
-    const { title, icon } = this.props;
-
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={icon} /> {title}
-        </h1>
-      </nav>
-    );
-  }
+function Havbar({ title, icon }) {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
 }
+
+Havbar.propTypes = {
+  title: PropTypes.string,
+  icon: PropTypes.string
+};
+
+Havbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+};
 
 export default Havbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
-
-    return (
-      <div className='card text-center'>
-        <img
-          className='round-img'
-          style={{ width: '60px' }}
-          src={avatar_url}
-          alt=''
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+function UserItem({ user: { avatar_url, login, html_url } }) {
+  return (
+    <div className='card text-center'>
+      <img
+        className='round-img'
+        style={{ width: '60px' }}
+        src={avatar_url}
+        alt=''
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+UserItem.propTypes = {
+  user: PropTypes.shape({
+    avatar_url: PropTypes.string.isRequired,
+    login: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default UserItem;
