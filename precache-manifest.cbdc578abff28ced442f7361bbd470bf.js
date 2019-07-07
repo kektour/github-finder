7372b3582585842c801a97ae680ad2fa6df5c20e
Alexey <elexex97@gmail.com>
2019-07-07 23:27:42 +0300

    Updates

---
+++ precache-manifest.cbdc578abff28ced442f7361bbd470bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d8dcd4c7175bc0e755edaee71f3880",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "f26b6b4b14615451af92",
+    "url": "/github-finder/static/css/main.ae8ec85a.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/github-finder/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "f26b6b4b14615451af92",
+    "url": "/github-finder/static/js/main.1e152611.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  }
+]);
