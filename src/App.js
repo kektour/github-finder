f11c27ced773a881026d3b5b3a457a1a29667bf6
Alexey <elexex97@gmail.com>
2019-07-07 17:02:45 +0300

    FEATURE: HTTP Requests & Updating State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import Havbar from './components/layout/Havbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({
+      users: res.data,
+      loading: false
+    });
+  }
+
   render() {
+    const { loading, users } = this.state;
+
     return (
       <div className='App'>
         <Havbar />
         <div className='container'>
-          <Users />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
