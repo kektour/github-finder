430c03b42a41e10ca78712ba441cdd8fba9c89ca
Alexey <elexex97@gmail.com>
2019-07-07 23:44:28 +0300

    FEATURE: Prepare & Deploy To gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Havbar from './components/layout/Havbar';
 import Home from './components/pages/Home';
 import User from './components/users/User';
